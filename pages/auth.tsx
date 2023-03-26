47c9753dec36b1c86075867e4c9710fe0eabc278
ergunDevv <ergncyln2127088@gmail.com>
2023-03-26 11:11:11 +0300

    Auth backend added

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,17 +1,31 @@
 import { useCallback, useState } from "react";
 import Input from "../components/input";
+import axios from "axios";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
 
-  const [variant, setVariant] = useState('login');
+  const [variant, setVariant] = useState("login");
 
-  const toggleVariant = useCallback(()=>{
-    setVariant((currentVariant)=> currentVariant === "login" ? "register" : "login")
-  },[])
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) =>
+      currentVariant === "login" ? "register" : "login"
+    );
+  }, []);
 
+  const register = useCallback(async () => {
+    try {
+      await axios.post('/api/register',{
+        email,
+        name,
+        password,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -22,16 +36,16 @@ const Auth = () => {
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
             <h2 className="text-white text-4xl mb-8 font-semibold">
-              {variant === 'login' ? 'Sign in' : 'Register'}
+              {variant === "login" ? "Sign in" : "Register"}
             </h2>
             <div className="flex flex-col gap-4">
-              {variant === 'register' && (
+              {variant === "register" && (
                 <Input
                   id="name"
                   type="text"
                   label="Username"
                   value={name}
-                  onChange={(e: any) => setName(e.target.value)} 
+                  onChange={(e: any) => setName(e.target.value)}
                 />
               )}
               <Input
@@ -39,25 +53,28 @@ const Auth = () => {
                 type="email"
                 label="Email address or phone number"
                 value={email}
-                onChange={(e: any) => setEmail(e.target.value)} 
+                onChange={(e: any) => setEmail(e.target.value)}
               />
               <Input
-                type="password" 
-                id="password" 
-                label="Password" 
+                type="password"
+                id="password"
+                label="Password"
                 value={password}
-                onChange={(e: any) => setPassword(e.target.value)} 
+                onChange={(e: any) => setPassword(e.target.value)}
               />
             </div>
             <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 translate">
-              {variant ==="login" ? "Login" : "Sign Up"}
+              {variant === "login" ? "Login" : "Sign Up"}
             </button>
             <p className="text-neutral-500 mt-12 ">
-              { variant === "login" ? "First time using Netflix ? " : "Already have an account ? "}
-              <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
-                
-                { variant === "login" ? "Create an account" : "Login"}
-
+              {variant === "login"
+                ? "First time using Netflix ? "
+                : "Already have an account ? "}
+              <span
+                onClick={toggleVariant}
+                className="text-white ml-1 hover:underline cursor-pointer"
+              >
+                {variant === "login" ? "Create an account" : "Login"}
               </span>
             </p>
           </div>
