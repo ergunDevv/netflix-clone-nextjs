eb6b1799c1abc827cb4a54571ea9239b3c3129b4
ergunDevv <ergncyln2127088@gmail.com>
2023-03-28 11:11:11 +0300

    google and github oath added

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -93,7 +93,7 @@ const Auth = () => {
             </button>
             <div className="flex flex-row items-center gap-4 mt-8 justify-center">
               <div
-                onClick={() => signIn("github", { callbackUrl: "/" })}
+                onClick={() => signIn("google", { callbackUrl: "/" })}
                 className=" w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
               >
                 <FcGoogle size={30} />
