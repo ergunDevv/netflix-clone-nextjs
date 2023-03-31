3de981e692b4faab6667ac89bbefa3ee76f21c12
ergunDevv <ergncyln2127088@gmail.com>
2023-03-31 11:11:11 +0300

    Navbar added

---
+++ components/Navbar.tsx
@@ -0,0 +1,59 @@
+import { useCallback, useState } from "react";
+import MobileMenu from "./MobileMenu";
+import NavbarItem from "./NavbarItem";
+
+import { BsChevronDown ,BsSearch,BsBell} from "react-icons/bs";
+import AccountMenu from "./AccountMenu";
+
+const Navbar = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+  const toggleMobileMenu = useCallback(() => {
+    setShowMobileMenu((current) => !current);
+  }, []);
+
+  return (
+    <div className="w-full fixed z-40 ">
+      <div className="px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 bg-zinc-900/90">
+        <img src="/images/logo.png" alt="logo" className="h-4 lg:h-7" />
+        <div
+          className="
+            flex-row ml-8 gap-7 hidden lg:flex
+            "
+        >
+          <NavbarItem label="Home" />
+          <NavbarItem label="Series" />
+          <NavbarItem label="Films" />
+          <NavbarItem label="New & Popular" />
+          <NavbarItem label="My List" />
+          <NavbarItem label="Browse by languages" />
+        </div>
+        <div
+          onClick={toggleMobileMenu}
+          className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative"
+        >
+          <p className="text-white text-sm">Browse</p>
+          <BsChevronDown className="text-white transition" />
+          <MobileMenu visible={showMobileMenu} />
+        </div>
+        <div className="flex flex-row ml-auto gap-7 items-center">
+          <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
+            <BsSearch/>
+          </div>
+          <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
+            <BsBell/>
+          </div>
+          <div className="flex flex-row items-center gap-2 cursor-pointer relative">
+                <div className="w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden">
+                    <img src="/images/default-blue.png" alt="" />
+                </div>
+                <BsChevronDown className="text-white transition" />
+                <AccountMenu/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { NextPageContext } from 'next'
 import {getSession, signOut} from 'next-auth/react'
 import useCurrentUser from '../hooks/useCurrentUser'
+import Navbar from '../components/Navbar'
 
 export async function getServerSideProps(context:NextPageContext) {
   const session = await getSession(context)
@@ -26,10 +27,7 @@ export default function Home() {
   
   return (
     <>
-      <h1 className='text-4xl text-green-500'>Netflix Clone</h1>
-      <p className='text-white'>Logged in as : {user?.name}</p>
-      <button className='h-10 w-full bg-white' onClick={()=>signOut()}>Logout!</button>
-      
+      <Navbar />
     </>
   )
 }
