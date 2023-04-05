a6cc75de8f91e39cc4e972e344fce28ca596b85f
ergunDevv <ergncyln2127088@gmail.com>
2023-04-05 11:11:11 +0300

    Favorites functionality backend added

--- pages/api/favorite.ts
+++ pages/api/favorite.ts
@@ -56,6 +56,7 @@ export default async function handler(
       });
       return res.status(200).json(updatedUser);
     }
+    return res.status(405).end();
   } catch (error) {
     console.log(error);
     return res.status(400).end();
