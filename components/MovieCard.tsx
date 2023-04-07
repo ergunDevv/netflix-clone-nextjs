8c93b510803310278c0bebd2e21ab3d1559a1970
ergunDevv <ergncyln2127088@gmail.com>
2023-04-07 11:11:11 +0300

    Play component and watch route added

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -2,11 +2,14 @@ import { Movie } from "@prisma/client";
 import React from "react";
 import { BsFillPlayFill } from "react-icons/bs";
 import FavoriteButton from "./FavoriteButton";
+import { useRouter } from "next/router";
 interface MovieCardProps {
   data: Record<string, any>;
 }
 
 const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
+  const router = useRouter();
+
   return (
     <div className="group bg-zinc-900 col-span relative h-[12vw]">
       <img
@@ -28,11 +31,13 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
           <div className="flex flex-row items-center gap-3">
             <div
               className="cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300"
-              onClick={() => {}}
+              onClick={() => {
+                router.push(`/watch/${data?.id}`);
+              }}
             >
               <BsFillPlayFill size={30} />
             </div>
-            <FavoriteButton movieId={data?.id}/>
+            <FavoriteButton movieId={data?.id} />
           </div>
           <p className="text-green-400 font-semibold mt-4">
             New
