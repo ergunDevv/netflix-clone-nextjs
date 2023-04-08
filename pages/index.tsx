7f359ccae0b35920d95641f6bdc9b43dcfd9fbb8
ergunDevv <ergncyln2127088@gmail.com>
2023-04-08 11:11:11 +0300

    Info box added to billboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,8 @@ import Billboard from "../components/Billboard";
 import MovieList from "../components/MovieList";
 import useMovieList from "../hooks/useMovieList";
 import useFavorites from "../hooks/useFavorites";
+import InfoModal from "../components/InfoModal";
+import useInfoModalStore from "../hooks/useInfoModalStore";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -26,9 +28,11 @@ export async function getServerSideProps(context: NextPageContext) {
 export default function Home() {
   const { data: movies = [] } = useMovieList();
   const { data: favorites = [] } = useFavorites();
+  const {isOpen, closeModal} = useInfoModalStore();
 
   return (
     <>
+      <InfoModal visible={isOpen} onClose={closeModal} />
       <Navbar />
       <Billboard />
       <div className="pb-40">
